eae7f2e68f1e38aa77b46c8bb488b3714dd1c1ed
Kshitiz Kamal <skilite007@gmail.com>
2024-01-03 00:43:10 +0530

    update side navigation

--- lib/presentation/core/widgets/sidebar.dart
+++ lib/presentation/core/widgets/sidebar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:vuexy_flutter/generated/l10n/app_localizations.dart';
+import 'package:vuexy_flutter/presentation/core/widgets/sidebar_tile.dart';
 
 class SideBar extends StatelessWidget {
   const SideBar({super.key});
@@ -10,18 +11,121 @@ class SideBar extends StatelessWidget {
       width: 300,
       child: ListView(
         children: [
-          ExpansionTile(
-            leading: const Icon(Icons.home_outlined),
-            title: Text(AppLocalizations.of(context).dashboards),
+          SidebarTile(
+            icon: const Icon(Icons.home_outlined),
+            title: AppLocalizations.of(context).dashboards,
             children: [
-              ListTile(
-                title: Text(AppLocalizations.of(context).analytics),
+              SidebarTile(
+                title: AppLocalizations.of(context).analytics,
               ),
+              SidebarTile(title: AppLocalizations.of(context).crm),
+              SidebarTile(title: AppLocalizations.of(context).ecommerce),
+              SidebarTile(title: AppLocalizations.of(context).academy),
+              SidebarTile(title: AppLocalizations.of(context).logistics),
             ],
           ),
-          ExpansionTile(
-            title: Text(AppLocalizations.of(context).frontPages),
+          SidebarTile(
+            title: AppLocalizations.of(context).frontPages,
+            children: [
+              SidebarTile(title: AppLocalizations.of(context).landing),
+              SidebarTile(title: AppLocalizations.of(context).pricing),
+              SidebarTile(title: AppLocalizations.of(context).payment),
+              SidebarTile(title: AppLocalizations.of(context).checkout),
+              SidebarTile(title: AppLocalizations.of(context).helpCenter),
+            ],
           ),
+          SidebarTile(
+            title: AppLocalizations.of(context).ecommerce,
+            children: [
+              SidebarTile(title: AppLocalizations.of(context).dashboard),
+              SidebarTile(
+                title: AppLocalizations.of(context).product,
+                children: [
+                  SidebarTile(title: AppLocalizations.of(context).list),
+                  SidebarTile(title: AppLocalizations.of(context).add),
+                  SidebarTile(title: AppLocalizations.of(context).category),
+                ],
+              ),
+              SidebarTile(
+                title: AppLocalizations.of(context).order,
+                children: [
+                  SidebarTile(title: AppLocalizations.of(context).list),
+                  SidebarTile(title: AppLocalizations.of(context).details),
+                ],
+              ),
+              SidebarTile(
+                title: AppLocalizations.of(context).customer,
+                children: [
+                  SidebarTile(title: AppLocalizations.of(context).list),
+                  SidebarTile(title: AppLocalizations.of(context).details),
+                ],
+              ),
+              SidebarTile(title: AppLocalizations.of(context).manageReview),
+              SidebarTile(title: AppLocalizations.of(context).referrals),
+              SidebarTile(title: AppLocalizations.of(context).settings),
+            ],
+          ),
+          SidebarTile(
+            title: AppLocalizations.of(context).academy,
+            children: [
+              SidebarTile(title: AppLocalizations.of(context).dashboard),
+              SidebarTile(title: AppLocalizations.of(context).myCourse),
+              SidebarTile(title: AppLocalizations.of(context).courseDetails),
+            ],
+          ),
+          SidebarTile(
+            title: AppLocalizations.of(context).logistics,
+            children: [
+              SidebarTile(title: AppLocalizations.of(context).dashboard),
+              SidebarTile(title: AppLocalizations.of(context).fleet),
+            ],
+          ),
+          SidebarTile(title: AppLocalizations.of(context).email),
+          SidebarTile(title: AppLocalizations.of(context).chat),
+          SidebarTile(title: AppLocalizations.of(context).calendar),
+          SidebarTile(
+            title: AppLocalizations.of(context).invoice,
+            children: [
+              SidebarTile(title: AppLocalizations.of(context).list),
+              SidebarTile(title: AppLocalizations.of(context).preview),
+              SidebarTile(title: AppLocalizations.of(context).edit),
+              SidebarTile(title: AppLocalizations.of(context).add),
+            ],
+          ),
+          SidebarTile(
+            title: AppLocalizations.of(context).user,
+            children: [
+              SidebarTile(title: AppLocalizations.of(context).list),
+              SidebarTile(title: AppLocalizations.of(context).preview),
+            ],
+          ),
+          SidebarTile(
+            title: AppLocalizations.of(context).rolesAndPermissions,
+            children: [
+              SidebarTile(title: AppLocalizations.of(context).roles),
+              SidebarTile(title: AppLocalizations.of(context).permissions),
+            ],
+          ),
+          SidebarTile(
+            title: AppLocalizations.of(context).pages,
+            children: [
+              SidebarTile(title: AppLocalizations.of(context).userProfile),
+              SidebarTile(title: AppLocalizations.of(context).accountSettings),
+              SidebarTile(title: AppLocalizations.of(context).pricing),
+              SidebarTile(title: AppLocalizations.of(context).faq),
+              SidebarTile(
+                title: AppLocalizations.of(context).miscellaneous,
+                children: [
+                  SidebarTile(title: AppLocalizations.of(context).comingSoon),
+                  SidebarTile(
+                      title: AppLocalizations.of(context).underMaintenance),
+                  SidebarTile(title: AppLocalizations.of(context).pageNotFound),
+                  SidebarTile(
+                      title: AppLocalizations.of(context).notAuthorized),
+                ],
+              )
+            ],
+          )
         ],
       ),
     );
--- lib/presentation/core/widgets/sidebar_tile.dart
+++ lib/presentation/core/widgets/sidebar_tile.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:vuexy_flutter/generated/l10n/app_localizations.dart';
 
 class SidebarTile extends StatelessWidget {
-  const SidebarTile({super.key});
+  const SidebarTile({
+    super.key,
+    this.children,
+    required this.title,
+    this.icon,
+  });
+
+  final String title;
+  final Icon? icon;
+  final List<SidebarTile>? children;
 
   @override
   Widget build(BuildContext context) {
-    return ExpansionTile(
-      leading: const Icon(Icons.home_outlined),
-      title: Text(AppLocalizations.of(context).dashboards),
-      children: [
-        ListTile(
-          title: Text(AppLocalizations.of(context).analytics),
-        ),
-      ],
-    );
+    final bool hasChildren = children != null && children!.isNotEmpty;
+    return hasChildren
+        ? ExpansionTile(
+            leading: icon,
+            title: Text(title),
+            children: children!,
+          )
+        : ListTile(
+            leading: icon,
+            title: Text(title),
+          );
   }
 }
